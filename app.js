dd9a7bba78ddd0455016f494314d20b9289d0f0f
Muhannad ibrahim <algodzilla2311@gmail.com>
2022-09-15 00:39:04 +0200

    update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const dotenv = require('dotenv')
 const bodyParser = require('body-parser');
-dotenv.config()
 const calcuRoutes = require('./src/routes/calcuRoutes')
+require('dotenv').config()
+
 app.use(express.json())
 app.use('/api/calculation',calcuRoutes)
 
